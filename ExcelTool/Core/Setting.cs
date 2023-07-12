01157586befd232520f9d688cae9aa43d68fac42
iningwei <iningwei@outlook.com>
2023-07-12 19:54:02 +0800

    x

--- ExcelTool/Core/Setting.cs
+++ ExcelTool/Core/Setting.cs
@@ -7,7 +7,7 @@ namespace ExcelTool.Core
 {
     class Setting
     {
-        public static bool IsEncrypt = false;//文件是否加密
+        public static bool IsEncrypt = true;//文件是否加密
         public static string EncryptKey = "goodluck";//文件加密密钥
         public static bool IsFileNameEncrypt = true;//最终导出的bin文件名是否加密
         public static string FileNameEncryptKey = "hijack88";//文件名加密密钥
