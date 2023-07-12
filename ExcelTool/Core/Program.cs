240c0663bb4cb6904147c8a2b0ba95c108945ea5
iningwei <iningwei@outlook.com>
2023-07-12 19:10:01 +0800

    add table_tmp folder

--- ExcelTool/Core/Program.cs
+++ ExcelTool/Core/Program.cs
@@ -16,6 +16,7 @@ namespace ExcelTool
         static string excelFileDir;
         static string[] excelFiles;
 
+        static string tmpExcelFileDir;//tmp excel file dir, files copy from excelFileDir
         static string outputTableDir;//the directory for outputing bin files
         static string outputCSCodeDir;//the directory for outputing c# code files
         static string outputLuaCodeDir;//the direcotry for lua code outputs
@@ -29,7 +30,7 @@ namespace ExcelTool
 
 
             //do not support 03 excel file,which with postfix of .xls
-            excelFiles = Directory.GetFiles(excelFileDir, "*.xlsx", SearchOption.TopDirectoryOnly);
+            excelFiles = Directory.GetFiles(tmpExcelFileDir, "*.xlsx", SearchOption.TopDirectoryOnly);
 
             List<ExcelTable> allExcelTables = new List<ExcelTable>();
             for (int q = 0; q < excelFiles.Length; q++)
@@ -58,42 +59,65 @@ namespace ExcelTool
 
         private static void init(string[] args)
         {
+            string curDir = Environment.CurrentDirectory;
+            binDir = curDir.Substring(0, curDir.LastIndexOf(@"\"));
             if (args.Length == 0)//if we not assign params
             {
-                string curDir = Environment.CurrentDirectory;
                 Debug.Log("curDir:" + curDir);
-                binDir = curDir.Substring(0, curDir.LastIndexOf(@"\"));
                 outputTableDir = binDir + @"\table_output";
                 outputCSCodeDir = binDir + @"\code_output_cs";
                 outputLuaCodeDir = binDir + @"\code_output_lua";
 
                 excelFileDir = binDir + @"\table";
+                tmpExcelFileDir = binDir + @"\table_tmp";
                 Debug.Log("excelFileDir:" + excelFileDir);
             }
             else
             {
-                if (args.Length != 4)
+                if (args.Length != 5)
                 {
-                    Debug.ThrowException("error,if you assign params then args.Length must be 4");
+                    Debug.ThrowException("error,if you assign params then args.Length must be 5");
                 }
                 else
                 {
-                    excelFileDir = args[0];
-                    outputTableDir = args[1];
-                    outputCSCodeDir = args[2];
-                    outputLuaCodeDir = args[3];
+                    excelFileDir = Path.Combine(binDir, args[0]);
+                    tmpExcelFileDir = Path.Combine(binDir, args[1]);
+                    outputTableDir = Path.Combine(binDir, args[2]);
+                    outputCSCodeDir = Path.Combine(binDir, args[3]);
+                    outputLuaCodeDir = Path.Combine(binDir, args[4]);
                     Debug.Log("excelFileDir:" + excelFileDir);
+                    Debug.Log("tmpExcelFileDir:" + tmpExcelFileDir);
                     Debug.Log("outputTableDir:" + outputTableDir);
                     Debug.Log("outputCSCodeDir:" + outputCSCodeDir);
                     Debug.Log("outputLuaCodeDir:" + outputLuaCodeDir);
-                    if (!Directory.Exists(excelFileDir)
-                        || !Directory.Exists(outputTableDir)
-                        || !Directory.Exists(outputCSCodeDir)
-                        || !Directory.Exists(outputLuaCodeDir))
+                    if (!Directory.Exists(excelFileDir))
                     {
-                        Debug.ThrowException("directory has error,please check");
+                        Debug.ThrowException("excelFileDir not exist：" + excelFileDir);
                     }
+                    if (!Directory.Exists(tmpExcelFileDir))
+                    {
+                        Directory.CreateDirectory(tmpExcelFileDir);
+                    }
+                }
+            }
 
+            //copy all files from excelFileDir to tmpExcelFileDir
+            //to avoid output conflict while .xlsx file is editing.
+            //we make tmpExcelFileDir  as the final xlsx files folder.
+            var excelFiles = Directory.GetFiles(excelFileDir, "*.xlsx", SearchOption.TopDirectoryOnly);
+            if (excelFiles.Length > 0)
+            {
+                for (int i = 0; i < excelFiles.Length; i++)
+                {
+                    var info = new FileInfo(excelFiles[i]);
+                    if (info.Name.StartsWith("~$"))//skip ~$ files
+                    {
+                        continue;
+                    }
+                    var from = excelFiles[i];
+                    var to = tmpExcelFileDir + "/" + info.Name;
+                    Debug.Log($"copy {from} to {to}");
+                    File.Copy(from, to, true);
                 }
             }
         }
